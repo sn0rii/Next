1d275899cda7b51b433ccb89b826ac42efa4ff3a
Wojtek M <ojciecneo@gmail.com>
2023-10-22 12:40:08 +0200

    replaceAll problem

---
+++ mini-project/my-mini-project/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import Navbar from "./components/Navbar";
+
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Mini Project Next",
+  description: "Wiki Rocket",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-slate-800">
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
